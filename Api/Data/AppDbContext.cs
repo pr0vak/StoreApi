100761cc69391f477dea09df177f5e6a8b9729fa
Vyacheslav <slava93omsk@mail.ru>
2025-01-09 11:55:16 +0600

    009 Наполнение тестовыми данными, использую библиотеку Bogus. Добавление миграции Seed-Products-List.

--- Api/Data/AppDbContext.cs
+++ Api/Data/AppDbContext.cs
@@ -1,16 +1,27 @@
 using Api.Models;
+using Api.Seed;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace Api.Data;
 
 public class AppDbContext : IdentityDbContext
 {
-    public AppDbContext(DbContextOptions options) : base(options)
-    {
-
-    }
+    public AppDbContext(DbContextOptions options) : base(options) { }
 
     public DbSet<AppUser> AppUsers { get; set; }
     public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<Product>().HasData(FakeProductGenerator.GenerateProductList());
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.ConfigureWarnings(warnings =>
+          warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
+    }
 }
---
+++ Api/Seed/FakeProductGenerator.cs
@@ -0,0 +1,26 @@
+using Api.Models;
+using Bogus;
+
+namespace Api.Seed;
+
+public static class FakeProductGenerator
+{
+    public static List<Product> GenerateProductList(int count = 10)
+    {
+        var products = new List<Product>();
+
+        var categories = new[] { "Категория 1", "Категория 2", "Категория 3" };
+        var specialTags = new[] { "Новинка", "Популярные", "Рекомендуемые" };
+
+
+        return new Faker<Product>("ru")
+            .RuleFor(m => m.Id, f => f.IndexFaker + 1)
+            .RuleFor(m => m.Name, f => f.Commerce.ProductName() + 1)
+            .RuleFor(m => m.Description, f => f.Lorem.Sentence() + 1)
+            .RuleFor(m => m.SpecialTag, f => f.PickRandom(specialTags))
+            .RuleFor(m => m.Category, f => f.PickRandom(categories))
+            .RuleFor(m => m.Price, f => Math.Round(f.Random.Decimal(1, 1000), 2))
+            .RuleFor(m => m.Image, f => "https://dummyimage.com/100x100/fff/aaa")
+            .Generate(count);
+    }
+}
