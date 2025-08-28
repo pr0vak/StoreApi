9f1a31a75676030fb79cf06db22fcbaf6033099d
Vyacheslav Kalin <slava93omsk@mail.ru>
2025-08-28 16:31:45 +0600

    006 Конфигурирование приложения для работы с базой данных

---
+++ Api/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data;
+
+public class AppDbContext : IdentityDbContext
+{
+    public AppDbContext(DbContextOptions options) 
+        : base(options)
+    {
+        
+    }
+}
