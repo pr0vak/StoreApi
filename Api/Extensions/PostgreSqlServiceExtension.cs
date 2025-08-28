11d52ebb9392d0df8c53e3f637edbe7db61bcb2c
Vyacheslav Kalin <slava93omsk@mail.ru>
2025-08-28 16:45:43 +0600

    007 Рефакторинг

---
+++ Api/Extensions/PostgreSqlServiceExtension.cs
@@ -0,0 +1,18 @@
+using Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Extensions;
+
+public static class PostgreSqlServiceExtension
+{
+    public static void AddPostgreSqlDbContext(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseNpgsql(
+                configuration.GetConnectionString("PostgreSQLConnection"));
+        });     
+    }
+}
