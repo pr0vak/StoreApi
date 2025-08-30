eda82febdfe64392414dcea491324173b5f45ba2
Vyacheslav Kalin <slava93omsk@mail.ru>
2025-08-30 19:21:37 +0600

    014 Endpoint получения продукта по ID

--- Api/Controllers/ProductController.cs
+++ Api/Controllers/ProductController.cs
@@ -21,5 +21,33 @@ public class ProductController : StoreController
             Result = await dbContext.Products.ToListAsync()
         });
     }
+
+    [HttpGet]
+    public async Task<IActionResult> GetProductById(int id)
+    {
+        if (id <= 0)
+            return BadRequest(new ServerResponse()
+            {
+                IsSuccess = false,
+                StatusCode = HttpStatusCode.BadRequest,
+                ErrorMessages = { "Некорректно введен Id продукта." }
+            });
+
+        var product = await dbContext.Products.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (product is null)
+            return NotFound(new ServerResponse()
+            {
+                IsSuccess = false,
+                StatusCode = HttpStatusCode.NotFound,
+                ErrorMessages = { "Продукт с таким Id не существует." }
+            });
+
+        return Ok(new ServerResponse()
+        {
+            StatusCode = HttpStatusCode.OK,
+            Result = product
+        });
+    }
 }
 
