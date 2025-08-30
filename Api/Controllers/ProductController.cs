0c9caa72992a286e8d90dbd34b9c0d2b16db0d33
Vyacheslav Kalin <slava93omsk@mail.ru>
2025-08-30 20:40:55 +0600

    016 Endpoint создания продукта

--- Api/Controllers/ProductController.cs
+++ Api/Controllers/ProductController.cs
@@ -49,5 +49,68 @@ public class ProductController : StoreController
             Result = product
         });
     }
+
+    [HttpPost]
+    public async Task<ActionResult<ServerResponse>> CreateProduct(
+        [FromBody] ProductCreateDto productCreateDto)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                if (productCreateDto.Image is null
+                    || productCreateDto.Image.Trim().Length == 0)
+                {
+                    return BadRequest(new ServerResponse()
+                    {
+                        IsSuccess = false,
+                        StatusCode = HttpStatusCode.BadRequest,
+                        ErrorMessages = { "Image не может быть пустым" }
+                    });
+                }
+                else
+                {
+                    Product product = new()
+                    {
+                        Name = productCreateDto.Name,
+                        Description = productCreateDto.Description,
+                        Category = productCreateDto.Category,
+                        SpecialTag = productCreateDto.SpecialTag,
+                        Price = productCreateDto.Price,
+                        Image = "https://placehold.co/200x150"
+                    };
+
+                    await dbContext.Products.AddAsync(product);
+                    await dbContext.SaveChangesAsync();
+
+                    var response = new ServerResponse()
+                    {
+                        StatusCode = HttpStatusCode.Created,
+                        Result = product
+                    };
+
+                    return CreatedAtRoute(nameof(GetProductById), new { id = product.Id }, response);
+                }
+            }
+            else
+            {
+                return BadRequest(new ServerResponse()
+                {
+                    IsSuccess = false,
+                    StatusCode = HttpStatusCode.BadRequest,
+                    ErrorMessages = { "Данные модели не валидны" }
+                });
+            }
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new ServerResponse()
+            {
+                IsSuccess = false,
+                StatusCode = HttpStatusCode.BadRequest,
+                ErrorMessages = { "Что-то пошло не так...", ex.Message }
+            });
+        }
+    }
 }
 
