97a9dfc0562a0a70b1a50c639c4ffdc09477ad0d
Vyacheslav Kalin <slava93omsk@mail.ru>
2025-09-17 18:41:27 +0600

    025 DTO-модели для входа в систему

---
+++ Api/ModelDto/LoginRequestDto.cs
@@ -0,0 +1,7 @@
+namespace Api.ModelDto;
+
+public class LoginRequestDto
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
---
+++ Api/ModelDto/LoginResponseDto.cs
@@ -0,0 +1,7 @@
+namespace Api.ModelDto;
+
+public class LoginResponseDto
+{
+    public string Email { get; set; }
+    public string Token { get; set; }
+}
