08daedd83142b13336a98ac73051f2f015745820
Vyacheslav Kalin <slava93omsk@mail.ru>
2025-10-05 13:28:37 +0600

    034 Модель деталей заказа

---
+++ Api/Models/OrderDetails.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api.Models;
+
+public class OrderDetails
+{
+    [Key]
+    public int OrderDetailId { get; set; }
+    [Required]
+    public int OrderHeaderId { get; set; }
+    [Required]
+    public int ProductId { get; set; }
+    [ForeignKey("ProductId")]
+    public Product Product { get; set; }
+    [Required]
+    public int Quantity { get; set; }
+    [Required]
+    public string ItemName { get; set; }
+    [Required]
+    public decimal Price { get; set; }
+}
