a23791aa50776aa0b58f9d014077179ce547b638
Vyacheslav <slava93omsk@mail.ru>
2025-11-05 07:05:11 +0600

    040 Логика формирования заказа

---
+++ Api/ModelDto/OrderHeaderCreateDto.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.ModelDto;
+
+public class OrderHeaderCreateDto
+{
+    [Required]
+    public string CustomerName { get; set; }
+    [Required]
+    public string CustomerEmail { get; set; }
+    public string AppUserId { get; set; }
+    public decimal OrderTotalAmount { get; set; }
+    public string Status { get; set; }
+    public int TotalCount { get; set; }
+    public IEnumerable<OrderDetailsCreateDto> OrderDetailsDto { get; set; }
+}
--- Api/Services/OrdersService.cs
+++ Api/Services/OrdersService.cs
@@ -1,4 +1,6 @@
 using Api.Data;
+using Api.ModelDto;
+using Api.Models;
 
 namespace Api.Services;
 
@@ -9,5 +11,42 @@ public class OrdersService
     public OrdersService(AppDbContext dbContext)
     {
         this.dbContext = dbContext;
+    }
+
+    public async Task<OrderHeader> CreateOrderAsync(
+        OrderHeaderCreateDto orderHeaderCreateDto)
+    {
+        var order = new OrderHeader
+        {
+            CustomerName = orderHeaderCreateDto.CustomerName,
+            CustomerEmail = orderHeaderCreateDto.CustomerEmail,
+            AppUserId = orderHeaderCreateDto.AppUserId,
+            OrderTotalAmount = orderHeaderCreateDto.OrderTotalAmount,
+            TotalCount = orderHeaderCreateDto.TotalCount,
+            Status = string.IsNullOrWhiteSpace(orderHeaderCreateDto.Status)
+                ? SharedData.Statuses.Pending
+                : orderHeaderCreateDto.Status
+        };
+
+        await dbContext.OrderHeaders.AddAsync(order);
+        await dbContext.SaveChangesAsync();
+
+        foreach (var orderDetailsDto in orderHeaderCreateDto.OrderDetailsDto)
+        {
+            var orderDetails = new OrderDetails
+            {
+                OrderHeaderId = order.OrderHeaderId,
+                ProductId = orderDetailsDto.ProductId,
+                Quantity = orderDetailsDto.Quantity,
+                ItemName = orderDetailsDto.ItemName,
+                Price = orderDetailsDto.Price
+            };
+
+            await dbContext.OrderDetails.AddAsync(orderDetails);
+        }
+
+        await dbContext.SaveChangesAsync();
+
+        return order;
     }
 }
